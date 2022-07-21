084b225a6cde8729ff0f721f435ba934e242bc14
citer98 <101625256+citer98@users.noreply.github.com>
2022-07-21 18:51:00 +0700

    Update Hitung.php

--- Hitung.php
+++ Hitung.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Home</title>
+    <title>Penghitung</title>
         <style type="text/css">
     .element {
         position: relative;
         top: -190px;
         font-size: 20px;
+     }
+
+     .menu {
+        position: relative;
+        left: -200px;
+        font-size: 20px;  
+        top: 50px;
+     }
+
+     .title
+     {
+        position: relative;
+        font-size: 80px;  
+        top: -30px;
      }
 
      .hitung
@@ -26,7 +40,51 @@
 </head>
 <body>
         <br><br><br><br><br>
+        <table bgcolor="white" align="center" border="2" cellpadding="100">
+        <tr bgcolor="white">
+            <a name="top"></a>            
+            <th>
 
+            <div class="title">
+            Program Pertamina
+            </div>
+
+            <div class="menu">
+                <button>
+                    <img src="HOME.png" height="20" width="20">
+                        <a href="index.html"style="text-decoration:none">
+                            <b> HOME</b>
+                        </a>
+                    </button>
+                <button>
+                    <img src="sym.png" height="20" width="20">
+                    <a href="Hitung.php"style="text-decoration:none">
+                        <b> PENGHITUNG</b>
+                    </a>
+                </button>
+                <button>
+                    <img src="tab.png" height="20" width="20">
+                    <a href="Tampil.php"style="text-decoration:none">
+                        <b> LAPORAN</b>
+                    </a>
+                </button>
+                <button>
+                    <img src="PERSON.png" height="20" width="20">
+                    <a href="About.html"style="text-decoration:none">
+                        <b> ABOUT</b>
+                    </a>
+                </button>
+            </div>   
+            </th>
+        </tr>
+
+        <tr>
+            <td>
+                <div align="center">
+                    <strong>
+                        <font size="10" face="Courier New, Courier, mono">LAPORAN PEMBELIAN</font>
+                    </strong>
+                    <br>
           <?php
             //buka file counter mode baca
             $filecounter="counter.txt";
@@ -96,5 +154,11 @@
                             <center><a href="lihat.php">::Lihat Data Laporan::</a></center>
                         </strong>
                 </div>
+            </td>
+        </tr>
+        <tr>
+            
+        </tr>
+    </table>
 </body>
 </html>
